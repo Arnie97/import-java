88a34f15ea1a4c0a0033e29b7dd58ef7f1eff0b3
Arnie97 <arnie97@gmail.com>
2018-04-26 15:13:04 +0800

    Override __getattribute__ instead of __getattr__.

--- java.py
+++ java.py
@@ -1,18 +1,42 @@
+import atexit
 import builtins
-import pprint
+import os
 import types
 
 import javabridge as jb
+
+
+class JavaPackage(types.ModuleType):
+    'An on-demand Java class loader.'
+
+    def __getattribute__(self, attr):
+        if attr.startswith('__'):
+            return super().__getattribute__(attr)
+
+        env = jb.get_env()
+        if not env:
+            jb.start_vm(class_path=class_path())
+            atexit.register(jb.kill_vm)
+            env = jb.get_env()
+
+        class_name = self.__name__ + '.' + attr
+        if env.find_class(class_name.replace('.', '/')):
+            return jb.JClassWrapper(class_name)
+        else:
+            raise ModuleNotFoundError(class_name)
+
+
+def class_path():
+    'Read the $CLASSPATH environment variable.'
+    return os.environ.get('CLASSPATH', '').split(os.pathsep) + jb.JARS
 
 
 def new_import(name, *args, **kwargs):
     try:
         module = old_import(name, *args, **kwargs)
     except ModuleNotFoundError as e:
-        pprint.pprint(args)
-        module = types.ModuleType(name)
-        module.__getattr__ = lambda cls: \
-            jb.JClassWrapper(self.__name__ + '.' + attr)
+        module = JavaPackage(name)
+        print(dir(module))
     finally:
         return module
 
