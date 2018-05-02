4a8d8e0ca6118743f16a9aa87b18bb11a73818bf
Arnie97 <arnie97@gmail.com>
2018-05-02 15:51:58 +0800

    Accept failed doctests when testing with JavaBridge.

--- conftest.py
+++ conftest.py
@@ -1,8 +1,20 @@
-def pytest_unconfigure(config):
-    'Shut down the JVM instance after tests.'
-    try:
-        from javabridge import kill_vm
-    except ImportError:
-        pass
-    else:
+import pytest
+from _pytest.doctest import DoctestItem
+
+
+try:
+    from javabridge import kill_vm
+
+except ImportError:
+    pass
+
+else:
+    def pytest_runtest_setup(item):
+        'Doctest failures expected due to a different repr.'
+        if isinstance(item, DoctestItem):
+            mark = dict(xfail=pytest.mark.xfail(run=True))
+            item.keywords._markers.update(mark)
+
+    def pytest_unconfigure(config):
+        'Shut down the JVM instance after tests.'
         kill_vm()
